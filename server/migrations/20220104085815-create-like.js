ba35f56f34ebc21eedb5e15771524ff263246ee2
emily-hong0129 <85302678+emily-hong0129@users.noreply.github.com>
2022-01-18 23:44:16 +0900

    upload file

---
+++ server/migrations/20220104085815-create-like.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('likes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.STRING
+      },
+      post_id: {
+        type: Sequelize.INTEGER
+      },
+      comment_id: {
+        type: Sequelize.STRING
+      },
+      like_count: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('likes');
+  }
+};
