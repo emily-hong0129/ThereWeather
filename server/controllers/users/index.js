888e4bfde77b002b7958daf331b5c4b464a61cee
emily-hong0129 <85302678+emily-hong0129@users.noreply.github.com>
2022-01-18 23:43:47 +0900

    upload file

---
+++ server/controllers/users/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  photo: require("./photo"),
+  signup: require("./signup"),
+  duplicationtest: require("./duplicationtest"),
+  auth: require("./auth"),
+  socialcheck: require("./socialcheck"),
+  findpw: require("./findpw")
+};
